87fd270145281e1d1d424709c83c702e651ffab3
0x0010b <felix.chacaltana.felix@gmail.com>
2021-11-02 02:47:07 -0500

    commit: Update Login

---
+++ ClinicManagementLite/WebPatient/Login.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebPatient.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="www/css/Login.css" rel="stylesheet">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="form-signin text-center">
+        <asp:Panel ID="viewError" class="alert alert-danger mb-4" role="alert" runat="server" Visible="false">
+            <asp:Label ID="lblErrorMessage" runat="server"></asp:Label>
+        </asp:Panel>
+
+        <img class="mb-4" src="www/img/clinic_logo.png" alt="" width="100" height="100">
+        <h1 class="h3 mb-3 fw-normal">Pacientes</h1>
+
+        <div class="form-floating">
+            <asp:TextBox ID="txtDni" type="text" placeholder="DNI" CssClass="form-control" runat="server" MaxLength="8"></asp:TextBox>
+            <label for="txtDni">DNI</label>
+        </div>
+        <div class="form-floating">
+            <asp:TextBox ID="txtPassword" type="password" placeholder="Contraseña" CssClass="form-control" runat="server"></asp:TextBox>
+            <label for="txtPassword">Contraseña</label>
+        </div>
+        
+        <asp:Button ID="btnLogin" CssClass="w-100 btn btn-lg btn-primary" runat="server" Text="Ingresar" OnClick="btnLogin_Click" />
+        <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
+    </div>
+</asp:Content>
---
+++ ClinicManagementLite/WebPatient/Login.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Text.RegularExpressions;
+using WebPatient.ProxyPatient;
+
+namespace WebPatient
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack && HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("Index.aspx");
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Regex.IsMatch(txtDni.Text, @"^[\d]{8}$"))
+                {
+                    throw new Exception("Ingrese un DNI valido.");
+                }
+
+                if (txtPassword.Text.Length <= 0)
+                {
+                    throw new Exception("Ingrese una contraseña valida.");
+                }
+
+                ServicePatientClient proxyPatient = new ServicePatientClient();
+                PatientBE patientBE = proxyPatient.Login(txtDni.Text, txtPassword.Text);
+                proxyPatient.Close();
+
+                FormsAuthentication.RedirectFromLoginPage(patientBE.Dni, true);
+            }
+            catch (Exception ex)
+            {
+                viewError.Visible = true;
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+    }
+}
